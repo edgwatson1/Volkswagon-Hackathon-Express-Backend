5eec6ab9fdb1242e49ab731796b23c78c3ca7830
Edward Watson <e@edwardwatson.com|>
2020-02-05 16:07:36 +0000

    add dbcreate dbseed etc from angelina

--- db/create.js
+++ db/create.js
@@ -1,5 +1,4 @@
-const connection = require('./config');
-
+const connection = require("./config");
 const Car = `
   CREATE TABLE IF NOT EXISTS car (
     id INT NOT NULL AUTO_INCREMENT,
@@ -8,8 +7,6 @@ const Car = `
     PRIMARY KEY (id)
   );
 `;
-
-
 const Trip = `
   CREATE TABLE IF NOT EXISTS trip (
     id INT NOT NULL AUTO_INCREMENT,
@@ -19,7 +16,25 @@ const Trip = `
     destination VARCHAR(255) NOT NULL,
     car_start_mileage INT,
     car_end_mileage INT,
+    car_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (car_id)
     REFERENCES car (id)
   );
+  `;
+connection.query(Car, err => {
+  if (err) {
+    console.log(err);
+    connection.end();
+  } else {
+    console.log("car created");
+    connection.query(Trip, err => {
+      if (err) {
+        console.log(err);
+        connection.end();
+      } else {
+        console.log("trip created");
+      }
+    });
+  }
+});

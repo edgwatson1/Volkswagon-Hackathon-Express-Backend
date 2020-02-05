e2fd8ae14d7630f0ed1420d9b7640c0f44d178ea
Angélina RIET <angelina.riet@yahoo.com>
2020-02-05 14:55:27 +0000

    db create drop and reset done

---
+++ create.js
@@ -0,0 +1,27 @@
+const connection = require('./config');
+
+const CAR = `
+  CREATE TABLE IF NOT EXISTS car (
+    id INT NOT NULL AUTO_INCREMENT,
+    plate VARCHAR(20) NOT NULL,
+    img_url VARCHAR(2083),
+    PRIMARY KEY (id)
+  );
+`;
+
+const Trip = `
+  CREATE TABLE IF NOT EXISTS trip (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    start_trip DATETIME NOT NULL,
+    end_trip DATETIME NOT NULL,
+    destination VARCHAR(255) NOT NULL,
+    car_start_mileage INT,
+    car_end_mileage INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (car_id)
+    REFERENCES car (id)
+  );
+`;
+
+

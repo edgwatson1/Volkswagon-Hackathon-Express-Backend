fb700ec9b018ca241053670c8e746d494c175f1f
Edward Watson <e@edwardwatson.com|>
2020-02-05 15:33:09 +0000

    fix npm run dev issues

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var tripsRouter = require("./routes/trips");
+var indexRouter = require("./routes/index");
 
 var app = express();
 
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/", indexRouter);
 app.use("/trips", tripsRouter);
 
 // catch 404 and forward to error handler
--- routes/trips.js
+++ routes/trips.js
@@ -3,13 +3,9 @@ var router = express.Router();
 
 const { getAllTrips, createTrip } = require("../controllers/trips-controller");
 
-/* GET users listing. */
-router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
-});
+/* GET trips listing. */
+router.get("/", getAllTrips);
 
-router.get("/getall", getAllTrips);
-
-router.get("/getall", createTrip);
+// router.get("/getall/create", createTrip);
 
 module.exports = router;

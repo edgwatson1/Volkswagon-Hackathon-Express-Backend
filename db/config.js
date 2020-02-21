1dee3dff82e9e540830322ba5e13e4b36b86993b
nuno <ngplbc@gmail.com>
2020-02-21 15:02:04 +0000

    add createPool

--- db/config.js
+++ db/config.js
@@ -10,7 +10,7 @@ const mysql = require("mysql");
 
 require("dotenv").config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
